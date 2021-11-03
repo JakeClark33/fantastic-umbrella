3bb6aaa6e023da411c910f9ed528194aa10bbe80
Zac Strangeway <zachary.strangeway@tasconline.com>
2021-11-03 13:16:19 -0500

    tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,97 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+    const tags = await Tag.findAll({
+      include: [
+        {
+          model: Product
+        }
+      ]
+    });
+    return res.json(tags);
+  } catch(err) {
+    console.log(err);
+    return res.status(400).json({ error: err.message });
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  try {
+    const tag = await Tag.findOne({
+      where: { id: req.params.id },
+      include: [
+        {
+          model: Product
+        }
+      ]
+    });
+
+    if (!tag) throw new Error('Unable to find tag');
+
+    return res.json(tag);
+  } catch(err) {
+    console.log(err);
+    return res.status(400).json({ error: err.message });
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async (req, res) => {
+  try {
+    const tag_name = req.body.tag_name;
+
+    if (!tag_name) throw new Error('Invalid request body.');
+
+    const tag = await Tag.create({
+      tag_name
+    });
+
+    if (!tag) throw new Error('Unable to create tag');
+
+    return res.json(tag);
+  } catch(err) {
+    console.log(err);
+    return res.status(400).json({ error: err.message });
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const tag_name = req.body.tag_name;
+
+    if (!tag_name) throw new Error('Invalid request body.');
+
+    const tag = await Tag.findOne({
+      where: { id: req.params.id },
+    });
+
+    if (!tag) throw new Error('Unable to create tag');
+
+    tag.set({
+      tag_name
+    });
+
+    return res.json(tag);
+  } catch(err) {
+    console.log(err);
+    return res.status(400).json({ error: err.message });
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const tag = await Tag.destroy({
+      where: { id: req.params.id },
+    });
+
+    if (!tag) throw new Error(`Unable to delete tag`);
+
+    return res.status(200).send();
+  } catch(err) {
+    console.log(err);
+    return res.status(400).json({ error: err.message });
+  }
 });
 
 module.exports = router;
