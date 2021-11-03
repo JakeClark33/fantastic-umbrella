2de7217440d82589162fc38468b15c7b24acf81c
Zac Strangeway <zachary.strangeway@tasconline.com>
2021-11-03 12:47:28 -0500

    setup models

--- models/index.js
+++ models/index.js
@@ -5,10 +5,19 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+});
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+});
 
 // Tags belongToMany Products (through ProductTag)
 
