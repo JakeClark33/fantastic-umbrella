00f5124ace7ec2a34bc4bd1043b1b8aee535d1d6
Zac Strangeway <zachary.strangeway@tasconline.com>
2021-11-03 12:47:37 -0500

    category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,97 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Product, Category, Tag, ProductTag } = require('../../models');
 
-// The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const categories = await Category.findAll({
+      include: [
+        {
+          model: Product
+        }
+      ]
+    });
+    return res.json(categories);
+  } catch(err) {
+    console.log(err);
+    return res.status(400).json({ error: err.message });
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const category = await Category.findOne({
+      where: { id: req.params.id },
+      include: [
+        {
+          model: Product
+        }
+      ]
+    });
+
+    if (!category) throw new Error('Unable to find category');
+
+    return res.json(category);
+  } catch(err) {
+    console.log(err);
+    return res.status(400).json({ error: err.message });
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const category_name = req.body.category_name;
+
+    if (!category_name) throw new Error('Invalid request body.');
+
+    const category = await Category.create({
+      category_name
+    });
+
+    if (!category) throw new Error('Unable to create category');
+
+    return res.json(category);
+  } catch(err) {
+    console.log(err);
+    return res.status(400).json({ error: err.message });
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const category_name = req.body.category_name;
+
+    if (!category_name) throw new Error('Invalid request body.');
+
+    const category = await Category.findOne({
+      where: { id: req.params.id },
+    });
+
+    if (!category) throw new Error('Unable to create category');
+
+    category.set({
+      category_name
+    });
+
+    return res.json(category);
+  } catch(err) {
+    console.log(err);
+    return res.status(400).json({ error: err.message });
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const category = await Category.destroy({
+      where: { id: req.params.id },
+    });
+
+    if (!category) throw new Error(`Unable to delete category`);
+
+    return res.status(200).send();
+  } catch(err) {
+    console.log(err);
+    return res.status(400).json({ error: err.message });
+  }
 });
 
 module.exports = router;
